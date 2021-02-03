5663257d28faae6df22270954735f1d9c52ee669
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2021-02-02 20:22:40 -0500

    forgot pyramid_info

---
+++ visual_lib/include/vilib/common/pyramid_info.h
@@ -0,0 +1,52 @@
+/*
+ * class to hold frame and pyramid information
+ * pyramid_info.h
+ *
+ * Copyright (c) 2019-2020 Balazs Nagy,
+ * Robotics and Perception Group, University of Zurich
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "vilib/common/frame.h"
+#include "vilib/common/types.h" // for pyramid desc
+
+#include <memory>
+
+namespace vilib {
+
+struct PyramidInfo {
+PyramidInfo(const std::shared_ptr<Frame> &f,
+            const image_pyramid_descriptor_t &p) :
+  frame(f), pyramid(p) {}
+  std::shared_ptr<Frame> frame;
+  image_pyramid_descriptor_t pyramid;
+};
+
+} // namespace vilib
